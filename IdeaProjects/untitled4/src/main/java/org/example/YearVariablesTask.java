a78758699670143e26b3cd676de3bea4f678debd
Hikl845 <freelancer198756@gmail.com>
2026-01-07 03:25:39 +0200

    Delete IdeaProjects/untitled4 directory

--- IdeaProjects/untitled4/src/main/java/org/example/YearVariablesTask.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-class DigitSplitter{
-    public int getFirstDigit(int number){
-        String str = Integer.toString(number);
-        char b = str.charAt(1);
-        return b;
-    }
-}
